8a2c87b4ac7590dd05a5e2f58ad3dccfc2f8993d
Queco SM <sersalazar.marioni@gmail.com>
2018-08-10 07:13:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,31 @@
 class Dog
+   def initialize
+      puts "Create an animal"
+   end
+   
+   def set_name(new_name)
+      @name = new_name
+   end
 
-def initialize(name)
-  @name = name
+   def name
+      @name
+   end
+
+   def name=(new_name)
+      @name = new_name
+   end
+   
+   def set_breed(new_breed)
+      @breed = new_breed
+   end
+
+   def breed
+      @breed
+   end
+
+   def breed=(new_breed)
+      @breed = new_breed
+   end
+   
 end
 
-def name= (new_name)
-  @name = new_name
-end
-
-def name
-  @name
-end
-
-end
